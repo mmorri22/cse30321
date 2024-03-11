f7bb55a6d53178a035c234232119d466adc0b0a2
Matthew Morrison <mmorri22@nd.edu>
2024-03-11 13:39:55 -0400

    VM Aware Programming Start

--- inclass/lec17/code/top_down_splay.c
+++ inclass/lec17/code/top_down_splay.c
@@ -5,36 +5,36 @@
  * 
  * This code is part of the public domain
  *
-  "Splay trees", or "self-adjusting search trees" are a simple and
+  "Splay splay_trees", or "self-adjusting search splay_trees" are a simple and
   efficient data structure for storing an ordered set.  The data
-  structure consists of a binary tree, without parent pointers, and no
+  structure consists of a binary splay_tree, without parent pointers, and no
   additional fields.  It allows searching, insertion, deletion,
   deletemin, deletemax, splitting, joining, and many other operations,
-  all with amortized logarithmic performance.  Since the trees adapt to
+  all with amortized logarithmic performance.  Since the splay_trees adapt to
   the sequence of requests, their performance on real access patterns is
-  typically even better.  Splay trees are described in a number of texts
+  typically even better.  Splay splay_trees are described in a number of texts
   and papers [1,2,3,4,5].
 
   The code here is adapted from simple top-down splay, at the bottom of
   page 669 of [3].  The chief modification here is that the splay operation 
-  works even if the   item being splayed is not in the tree, and even if the 
-  tree root of the tree is NULL.  
+  works even if the   item being splayed is not in the splay_tree, and even if the 
+  splay_tree root of the splay_tree is NULL.  
 
   So the line:
 
                               t = splay(insert_val, t);
 
-  causes it to search for item with key insert_val in the tree rooted at t.  
+  causes it to search for item with key insert_val in the splay_tree rooted at t.  
   If it's   there, it is splayed to the root.  If it isn't there, then the node put
   at the root is the last one before NULL that would have been reached in a
-  normal binary search for insert_val.  (It's a neighbor of insert_val in the tree.)  
+  normal binary search for insert_val.  (It's a neighbor of insert_val in the splay_tree.)  
   This   allows many other operations to be easily implemented, as shown below.
 
   [1] "Fundamentals of data structures in C", Horowitz, Sahni,
        and Anderson-Freed, Computer Science Press, pp 542-547.
   [2] "Data Structures and Their Algorithms", Lewis and Denenberg,
        Harper Collins, 1991, pp 243-251.
-  [3] "Self-adjusting Binary Search Trees" Sleator and Tarjan,
+  [3] "Self-adjusting Binary Search splay_trees" Sleator and Tarjan,
        JACM Volume 32, No 3, July 1985, pp 652-686.
   [4] "Data Structure and Algorithm Analysis", Mark Weiss,
        Benjamin Cummins, 1992, pp 119-130.
@@ -47,24 +47,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct Tree {
-    struct Tree* left;
-    struct Tree* right;
+typedef struct splay_tree {
+    struct splay_tree* left;
+    struct splay_tree* right;
     int item;
-}Tree;
+}splay_tree;
 
-Tree* splay(int insert_val, Tree* to_splay) {
+splay_tree* splay(int insert_val, splay_tree* to_splay) {
 
-    /* Simple top down splay, not requiring i to be in the tree t.  */
+    /* Simple top down splay, not requiring i to be in the splay_tree t.  */
     /* What it does is described above.                             */
-    Tree New; 
-    Tree *left_tree, *right_tree, *y;
+    splay_tree New; 
+    splay_tree *left_splay_tree, *right_splay_tree, *y;
 
     if (to_splay == NULL) 
         return to_splay;
 
     New.left = New.right = NULL;
-    left_tree = right_tree = &New;
+    left_splay_tree = right_splay_tree = &New;
 
     while(1) {
 
@@ -82,8 +82,8 @@ Tree* splay(int insert_val, Tree* to_splay) {
                     break;
             }
             
-            right_tree->left = to_splay;                               /* link right */
-            right_tree = to_splay;
+            right_splay_tree->left = to_splay;                               /* link right */
+            right_splay_tree = to_splay;
             to_splay = to_splay->left;
         } 
         else if (insert_val > to_splay->item) {
@@ -102,8 +102,8 @@ Tree* splay(int insert_val, Tree* to_splay) {
             }
 
             /* Link Left */
-            left_tree->right = to_splay;
-            left_tree = to_splay;
+            left_splay_tree->right = to_splay;
+            left_splay_tree = to_splay;
             to_splay = to_splay->right;
         } 
         else {
@@ -112,9 +112,9 @@ Tree* splay(int insert_val, Tree* to_splay) {
         }
     }
 
-    /* Assemble the new Splay tree */
-    left_tree->right = to_splay->left;
-    right_tree->left = to_splay->right;
+    /* Assemble the new Splay splay_tree */
+    left_splay_tree->right = to_splay->left;
+    right_splay_tree->left = to_splay->right;
     to_splay->left = New.right;
     to_splay->right = New.left;
 
@@ -123,8 +123,8 @@ Tree* splay(int insert_val, Tree* to_splay) {
 
 /* Matt, here is how bob sedgewick would have written this.          */
 /* It does the same thing.                                           */
-Tree* sedgewickized_splay(int i, Tree* t) {
-    Tree N, *l, *r, *y;
+splay_tree* sedgewickized_splay(int i, splay_tree* t) {
+    splay_tree N, *l, *r, *y;
     if (t == NULL) return t;
     N.left = N.right = NULL;
     l = r = &N;
@@ -148,12 +148,12 @@ Tree* sedgewickized_splay(int i, Tree* t) {
     return t;
 }
 
-Tree* insert(int insert_val, Tree* t) {
+splay_tree* insert(int insert_val, splay_tree* t) {
 
-    /* Insert insert_val into the tree t, unless it's already there.    */
-    /* Return a pointer to the resulting tree.                 */
+    /* Insert insert_val into the splay_tree t, unless it's already there.    */
+    /* Return a pointer to the resulting splay_tree.                 */
 
-    Tree* new = (Tree*) calloc (1, sizeof (Tree));
+    splay_tree* new = (splay_tree*) calloc (1, sizeof (splay_tree));
     if (new == NULL) {
         fprintf(stdout, "Ran out of space\n");
         exit(1);
@@ -180,7 +180,7 @@ Tree* insert(int insert_val, Tree* t) {
         return new;
     } 
     
-    /* We get here if it's already in the Tree*/
+    /* We get here if it's already in the splay_tree*/
     /* Don't add it again                      */
     else { 
         free(new);
@@ -188,19 +188,21 @@ Tree* insert(int insert_val, Tree* t) {
     }
 }
 
-Tree* delete(int delete_val, Tree* t) {
-/* Deletes i from the tree if it's there.               */
-/* Return a pointer to the resulting tree.              */
-    Tree* x;
+/* Deletes i from the splay_tree if it's there.               */
+/* Return a pointer to the resulting splay_tree.              */
+splay_tree* delete(int delete_val, splay_tree* t) {
 
     /* Dr. Morrison's Golden Rule of Pointers */
     if (t==NULL) 
         return NULL;
 
-    /* Splay the element to the top of the tree */
+
+    splay_tree* x;
+
+    /* Splay the element to the top of the splay_tree */
     t = splay(delete_val,t);
 
-    /* Item was found and splayed to the top of the tree */
+    /* Item was found and splayed to the top of the splay_tree */
     if (delete_val == t->item) {     
                   
         if (t->left == NULL) {
@@ -221,7 +223,7 @@ Tree* delete(int delete_val, Tree* t) {
 }
 
 int main( const int argc, const char* argv[] ) {
-/* A sample use of these functions.  Start with the empty tree,         */
+/* A sample use of these functions.  Start with the empty splay_tree,         */
 /* insert some stuff into it, and then delete it                        */
 
     if( argc != 3 ){
@@ -232,8 +234,8 @@ int main( const int argc, const char* argv[] ) {
     int num_items = atoi(argv[1]);
     int num_tests = atoi(argv[2]);
 
-    /* Design an empty tree */
-    Tree* root = NULL;
+    /* Design an empty splay_tree */
+    splay_tree* root = NULL;
 
     // Call push_front with as many as we want to insert
     clock_t start_inserts = clock();
@@ -272,7 +274,7 @@ int main( const int argc, const char* argv[] ) {
     double total_tests = (double)(end_tests - start_tests) / (double)(CLOCKS_PER_SEC * loop_tests);
     
     fprintf( stdout, "--------------------------------\n");
-    fprintf( stdout, "Splay Tree Size        : %d\n", num_items);
+    fprintf( stdout, "Splay splay_tree Size        : %d\n", num_items);
     fprintf( stdout, "Clocks Per Second      : %ld\n", CLOCKS_PER_SEC );
     fprintf( stdout, "Clocks for all inserts : %ld\n", end_inserts - start_inserts);
     fprintf( stdout, "Average time per insert: %.6lf us\n", total_inserts / (double)num_tests * 1000000 );
