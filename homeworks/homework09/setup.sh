5a5a3992e44c102fd9b594ea90e110f6bf8baf22
Matthew Morrison <mmorri22@nd.edu>
2024-03-14 18:33:08 -0400

    Homework 09 Rearrange

--- homeworks/homework09/setup.sh
+++ homeworks/homework09/setup.sh
@@ -1,4 +1,8 @@
-mkdir part4 part5 part6
+mkdir part3 part4 part5
+cd part3
+wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part3/simd.txt
+wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part3/python.txt
+cd ../part4
 cd part4
 wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part4/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part4/part4.c
