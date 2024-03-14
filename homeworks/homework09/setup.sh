f95b9c04cf4a088dba93d92ebae49890bad75d62
Matthew Morrison <mmorri22@nd.edu>
2024-03-14 11:28:31 -0400

    Homework 09 Script Update

--- homeworks/homework09/setup.sh
+++ homeworks/homework09/setup.sh
@@ -1,5 +1,9 @@
 mkdir part1 part2 part3
 cd part1
+wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part1/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part1/simd.c
+wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part1/simd.h
+wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework09/part1/test_simd.c
 cd ../part2
 cd ../part3
 cd ..
