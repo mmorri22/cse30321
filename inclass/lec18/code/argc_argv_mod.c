bb9c80d38fcccb21e5de78c3bd0ad345a8abab17
Matthew Morrison <mmorri22@nd.edu>
2024-04-05 08:43:01 -0400

    Lecture 24 Updated Immediate

--- inclass/lec18/code/argc_argv_mod.c
+++ inclass/lec18/code/argc_argv_mod.c
@@ -2,16 +2,10 @@
 #include <stdlib.h>
 
 
-int main( const int argc, const char* argv[] ){
+int main( int argc, char* argv[] ){
 
     int iter;
-    argc += 2;
-    for(iter = 0; iter < argc; ++iter){
-        fprintf( stdout, "argv[%d] = %s\n", iter, argv[iter] );
-    }
-
-    int limit = 38;  // 5, 15, 36, 36, 37
-     for(iter = argc; iter < limit; ++iter){
+    for(iter = argc; iter < argc + 10; ++iter){
         fprintf( stdout, "argv[%d] = %s\n", iter, argv[iter] );
     }   
 
