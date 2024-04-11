2b052f886920f1ea1186be7bf9c3c8b7da5a668b
Matthew Morrison <mmorri22@nd.edu>
2024-04-11 16:07:28 -0400

    DGEMM to student04 test

--- homeworks/homework08/setup.sh
+++ homeworks/homework08/setup.sh
@@ -1,4 +1,3 @@
 wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework08/dgemm_matrix.c
-wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework08/dgemm_matrix_parallel.c
 wget https://raw.githubusercontent.com/mmorri22/cse30321/main/homeworks/homework08/Makefile
 rm setup.sh
