abc57ee5f1e19de3610e1bb217075e878ebc8bae
Matthew Morrison <mmorri22@nd.edu>
2024-04-10 22:38:02 -0400

    Lecture 23 Update

---
+++ inclass/lec23/code/hello.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main () {
+    int x = 0; //Shared variable
+	
+	fprintf( stdout, "Total number of threads is %d\n",  omp_get_max_threads() );
+	
+	#pragma omp parallel
+	{
+		int tid = omp_get_thread_num(); 
+
+		//Private variable - Whatever x is *at that moment* will add 1
+		++x;
+		
+		fprintf(stdout, "Hello World from thread %d, x = %d at %p\n", tid, x, &x );
+		
+		if(tid==0) {
+            // Note the value of x that prints here will be the one that prints at the end
+			fprintf(stdout, "Result of tid 0: x = %d\n", x );		
+		}
+	}	
+
+    // 
+	fprintf(stdout, "Done with parallel segment. x = %d\n", x);
+	
+	return EXIT_SUCCESS;
+}
+
